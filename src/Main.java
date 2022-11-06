a16617604096c3f2ede1a4df46bb811a05d68cf1
Gen <jdk094602@gmail.com>
2022-11-07 00:51:34 +0300

    на проверку ДЗ1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ну вот мы и дома. Может бвть ...  !");
+        System.out.println("Ну вот мы и дома. Может быть ...  !");
     }
 }
